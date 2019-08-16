1d5d6ae5885ad994d26f149cd9c951b53452905e
Hoon Yang <ssang276@hanmail.net>
2019-08-16 12:24:34 +0900

    asdf

--- test-tar-performance.sh
+++ test-tar-performance.sh
@@ -1,6 +1,8 @@
 # gzip
 # archive (not comporess)
 # archive (without data/git/repositories)
+du -h -d 2 .tmp/ski-gitea-data
+
 time tar -zcvpf - .tmp/ski-gitea-data | wc -c | tee .tmp archive.log.0
 time tar -cvpf  - .tmp/ski-gitea-data | wc -c | tee .tmp archive.log.1
 time tar --exclude .tmp/ski-gitea-data/data/git -cvpf - .tmp/ski-gitea-data | wc -c | tee .tmp archive.log.2
